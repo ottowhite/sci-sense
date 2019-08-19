d6c056dcff0b0fb2978cd936b5730e43f4ec2ee2
ottowhite <whiteotto4@gmail.com>
2019-08-19 15:02:24 +0100

    working scoring system, locally storing answers (change this)

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q, ObjectDoesNotExist
 
 # querying the 
 try:
-    user_data = AppUser.objects.get(Q(username="ottowhite") & Q(password="password"))
+    user_data = AppUser.objects.get(Q(username="britannioj") & Q(password="MLG"))
 except ObjectDoesNotExist:
     user_data = None
 
@@ -15,15 +15,14 @@ def home(request):
         'user_data': user_data,
         'title': 'Home'
     }
-
     return render(request, 'main_app/home.html', context)
 
 def quiz(request):
-
     
+    # a queryset of 15 randomly ordered question objects from a range in the spec is passed
     context = {
         'user_data': user_data,
-        'question_data': Question.objects.filter(spec_point__range=(1.1, 1.2)).order_by("?")[:15],
+        'question_data': Question.objects.filter(spec_point__range=(1.2, 1.3)).order_by("?")[:3],
         'title': 'Do quiz'
     }
 
