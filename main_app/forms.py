39edaa59325f694789b940b476bf3a7007b87bb7
ottowhite <whiteotto4@gmail.com>
2019-08-23 13:30:47 +0100

    fix quiz query page, question duplicates in form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from main_app.models import Question
 
 class GenerateQuizForm(forms.Form):
-    starting_specification_point = forms.FloatField()
-    ending_specification_point = forms.FloatField()
+    # this form will be passed as context to the quiz view
+    # when referenced with form.as_p, it will be rendered
+    # as a paragraph
+
+    # loading all distinct spec points that exist in the database a lists of tuples - (human readable, passed value)
+    choices = [(x.spec_point, x.spec_point) for x in Question.objects.order_by("spec_point").distinct()]
+
+    starting_specification_point = forms.ChoiceField(choices=choices)
+    ending_specification_point = forms.ChoiceField(choices=choices)
     maximum_questions = forms.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -19,11 +19,15 @@ class HomeView(TemplateView):
 
 
     def get(self, request):
+        # this method is called upon the initial request for the webpage
+        
+        # the args are the different pieces of session/page data that the page will adapt to
         args = {
             'user_data': user_data,
             'title': 'Home'
         }
 
+        # the render method will combine the template with given context, then return generated HttpResponse
         return render(request, self.template_name, args)
 
 class GenerateQuizView(TemplateView):
@@ -48,7 +52,7 @@ class GenerateQuizView(TemplateView):
     def post(self, request):
         # This method handles the GET request upon intially viewing the page
 
-        # create a form on the page containing the post data
+        # create a form variable on the page containing the filled out post data
         form = GenerateQuizForm(request.POST)
 
         # defining some arguments to be interacted with on the page
@@ -58,29 +62,35 @@ class GenerateQuizView(TemplateView):
             'title': 'Generate quiz'
         }
 
+        # ensuring that the form inputs are valid
         if form.is_valid():
+            # retrieving the absolute url of the main quiz page through a reverse lookup based on the
+            # name defined on created url patterns
             base_url = reverse('main-quiz')
+
+            # converting a dict containing GET data into a querystring to be used in the quiz page request
             query_string = urlencode({
                 'starting_specification_point': form.cleaned_data['starting_specification_point'],
                 'ending_specification_point': form.cleaned_data['ending_specification_point'],
                 'maximum_questions': form.cleaned_data['maximum_questions']
             })
 
+            # creating a get request with parameters from the form
             return redirect(f'{base_url}?{query_string}')
-
-        return render(request, self.template_name, args)
 
 class QuizView(TemplateView):
     template_name = 'main_app/quiz.html'
 
     def get(self, request):
-        starting_specification_point = float(request.GET.get('starting_specification_point'))
-        ending_specification_point = float(request.GET.get('ending_specification_point')) - 0.001
-        maximum_questions = int(request.GET.get('maximum_questions'))
+        # retrieving the different form values from the address, casting to according data types
+        start = float(request.GET.get('starting_specification_point'))
+        end = float(request.GET.get('ending_specification_point'))
+        maximum = int(request.GET.get('maximum_questions'))
 
+        # also adds a randomly ordered queryset of given length within the given range, containing questions
         args = {
             'user_data': user_data,
-            'question_data': Question.objects.filter(spec_point__range=(starting_specification_point, ending_specification_point)).order_by("?")[:maximum_questions],
+            'question_data': Question.objects.filter(spec_point__range=(start, end)).order_by("?")[:maximum],
             'title': 'Do quiz'
         }
 
