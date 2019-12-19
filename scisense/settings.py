a58018d83de9bc3f951feadadb43de8bc3757725
ottowhite <whiteotto4@gmail.com>
2019-12-19 10:27:30 +0000

    install and use crispy forms on registration

--- scisense/settings.py
+++ scisense/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'main_app.apps.MainAppConfig',  # have to add this to tell django to search for templates
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 class RegisterView(TemplateView):
@@ -8,11 +9,30 @@ class RegisterView(TemplateView):
 
 
     def get(self, request):
-        form = UserCreationForm()
+        form = RegisterForm()
 
         args = {
             'title': 'Register',
             'form': form
         }
 
+        return render(request, self.template_name, args)
+    
+    def post(self, request):
+
+        # collects the POST request 
+        form = RegisterForm(request.POST)
+
+        # ensures that the form data conforms to certain validation checks
+        if form.is_valid():
+            username = form.cleaned_data.get('username') # retrieving the data
+            form.save() # saving to the DB
+            messages.success(request, f'Account created for {username}!') # adding message session variable
+            return redirect('main-home') # redirect to the home
+        else:
+            # if the form is invalid it will redirect to the same page with the populated form
+            args = {
+                'title': 'Register',
+                'form': form
+            }
         return render(request, self.template_name, args)
