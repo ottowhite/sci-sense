c941019ea64ce6bf6eb0a7dec75c42579ba0b82f
ottowhite <whiteotto4@gmail.com>
2019-12-19 11:23:17 +0000

    add login and logout pages, working

--- users/forms.py
+++ users/forms.py
@@ -4,10 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    institution_code = forms.CharField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     
     class Meta:
         # specifies that the model to be affected is the User model
         model = User
 
         # specifies the fields that will be displayed
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'institution_code']
